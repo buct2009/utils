33a5da3b9e6fe17d8b70f2cfd85a9ca7c4f724e2
Rundong Zhang <zhangrundong@shopee.com>
2023-07-18 16:26:00 +0800

    use utc as default timezone

--- database/row_in_map.go
+++ database/row_in_map.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"fmt"
+	"net/url"
 	"strconv"
 	"sync"
 	"time"
@@ -1348,8 +1349,8 @@ func getDataType(dbColType string) (colType string) {
 
 func (m *MySQL) fillConnStr() string {
 	dbServerInfoStr := fmt.Sprintf(
-		"%s:%s@tcp(%s:%d)/%s?multiStatements=%v&interpolateParams=%v&maxAllowedPacket=%d",
-		m.UserName, m.Passwd, m.IP, m.Port, m.DBName, m.MultiStatements, m.InterpolateParams, m.MaxAllowedPacket)
+		"%s:%s@tcp(%s:%d)/%s?multiStatements=%v&interpolateParams=%v&maxAllowedPacket=%d&time_zone=%s",
+		m.UserName, m.Passwd, m.IP, m.Port, m.DBName, m.MultiStatements, m.InterpolateParams, m.MaxAllowedPacket, url.QueryEscape("+00:00"))
 	if m.QueryTimeout > 0 {
 		dbServerInfoStr = fmt.Sprintf("%s&timeout=3s&readTimeout=%ds&writeTimeout=%ds",
 			dbServerInfoStr, m.QueryTimeout, m.QueryTimeout)
